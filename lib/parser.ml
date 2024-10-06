1512d80580965b7977dbac935d2cc3921513b490
ricardo <ricardorbxx1@gmail.com>
2024-10-05 22:12:25 -0300

    refactor: enhancing code readability, making it less cringe

--- lib/parser.ml
+++ lib/parser.ml
@@ -1,3 +1,5 @@
+let remove_last s = String.sub s 0 (String.length s - 1)
+
 let split_lines lines  =
   let rec aux list acc =
     match list with
@@ -7,28 +9,26 @@ let split_lines lines  =
         aux tl (words @ acc)
   in aux lines [];;
 
-let remove_last s = String.sub s 0 (String.length s - 1)
-
-let find_constructor words =
-    let rec aux list acc =
-    match list with
-    | [] -> acc 
-    | word :: rest -> 
-      match word with
+let find_constructor tokens =
+  let rec process_tokens tokens acc =
+    match tokens with
+    | [] -> acc
+    | token :: rest ->
+      match token with
       | "constructor(" -> 
-        let rec find_class class_words class_acc =
-          match class_words with
-          | [] -> aux rest class_acc
+        let rec extract_constructor_params params_acc remaining_tokens =
+          match remaining_tokens with
+          | [] -> process_tokens rest (List.rev params_acc @ acc)
           | hd :: tl -> 
             match hd with
-            | ")" -> class_acc @ aux rest acc
-            | "private" -> find_class tl class_acc
-            | "readonly" -> find_class tl class_acc
-            | _  -> remove_last hd :: find_class tl class_acc
-        in find_class rest []
-      | _ -> aux rest acc
-  in aux words [];;
-
+            | ")" -> process_tokens rest (List.rev params_acc @ acc)
+            | "private" | "readonly" -> extract_constructor_params params_acc tl
+            | _ -> extract_constructor_params (remove_last hd :: params_acc) tl
+        in
+        extract_constructor_params [] rest
+      | _ -> process_tokens rest acc
+  in
+  process_tokens tokens []
 
 let rec pair_up list =
   match list with
