e78f6e2e2c0619e4f506ade7b6be8d287638797a
ricardo <ricardorbxx1@gmail.com>
2024-10-06 17:25:20 -0300

    feat: add function to write to a file

--- lib/file.ml
+++ lib/file.ml
@@ -3,7 +3,15 @@ let read_file filename =
   let rec read_lines acc =
     try
       read_lines (input_line ic :: acc)
-    with End_of_file -> close_in ic;
-    List.rev acc
-    in read_lines []
+    with End_of_file -> 
+      close_in ic;
+      List.rev acc
+  in read_lines []
+;;
+
+
+let write_to_file ~filename ~message =
+  let oc = open_out_gen [Open_append; Open_creat] 0o666 filename in
+  Printf.fprintf oc "%s\n" message;
+  close_out oc;
 ;;
