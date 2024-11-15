976c74b456a6ecf56a10c23494118a82704a7a10
ricardo <ricardorbxx1@gmail.com>
2024-11-15 16:23:43 -0300

    remove target

--- target/debug/deps/t-13cfef72408f0de9.d
+++
@@ -1,5 +0,0 @@
-/home/rick/Projects/Repos/test_maker/target/debug/deps/libt-13cfef72408f0de9.rmeta: src/main.rs
-
-/home/rick/Projects/Repos/test_maker/target/debug/deps/t-13cfef72408f0de9.d: src/main.rs
-
-src/main.rs:
--- target/debug/deps/t-b522a9f328e400bf.d
+++
@@ -1,5 +0,0 @@
-/home/rick/Projects/Repos/test_maker/target/debug/deps/libt-b522a9f328e400bf.rmeta: src/main.rs
-
-/home/rick/Projects/Repos/test_maker/target/debug/deps/t-b522a9f328e400bf.d: src/main.rs
-
-src/main.rs:
