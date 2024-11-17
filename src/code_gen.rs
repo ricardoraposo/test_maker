491bcb5114a2a3c1b65c372eab58f88258394dff
ricardo <ricardorbxx1@gmail.com>
2024-11-17 11:30:38 -0300

    feat: module for file writing

---
+++ src/code_gen.rs
@@ -0,0 +1,53 @@
+use crate::parser::Module;
+use std::{fs::File, io::Write};
+
+pub fn write_header(mut buf: &File, header_name: &str) {
+    let header = format!(
+        "describe('{}', () => {{\n  let subject: {}\n",
+        header_name, header_name,
+    );
+    write!(buf, "{header}").unwrap()
+}
+
+pub fn write_let_statements(mut buf: &File, modules: &[Module]) {
+    for module in modules {
+        let st = format!("  let {}: {}\n", module.class.variable, module.class.name);
+        write!(buf, "{st}").unwrap()
+    }
+}
+
+pub fn write_mocks(mut buf: &File, modules: &[Module], main_class: &str) {
+    write!(buf, "\n  beforeEach(async () => {{\n    const module: TestingModule = await Test.createTestingModule({{\n      providers: [\n        {main_class},").unwrap();
+
+    for module in modules {
+        let st = format!(
+            "        {{\n          provide: {},\n          useValue: {{\n",
+            module.class.name
+        );
+        write!(buf, "{st}").unwrap();
+
+        for method in module.methods.clone() {
+            let st = format!("            {}: vi.fn(),\n", method);
+            write!(buf, "{st}").unwrap()
+        }
+        write!(buf, "          }},\n        }},\n").unwrap()
+    }
+    write!(buf, "      ],\n    }}).compile()\n").unwrap()
+}
+
+pub fn write_module_gets(mut buf: &File, modules: &[Module], main_class: &str) {
+    write!(buf, "    subject = module.get<{main_class}>({main_class})\n").unwrap();
+
+    for module in modules {
+        let st = format!(
+            "    {} = module.get<{}>({})\n",
+            module.class.variable, module.class.name, module.class.name
+        );
+        write!(buf, "{st}").unwrap()
+    }
+    write!(
+        buf,
+        "  }})\n\n  describe('these are gonna be really cool tests', () => {{}})\n}})"
+    )
+    .unwrap();
+}
